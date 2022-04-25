ab9efec7056ce79bcf6fa8edb87ac7a8bd6db4f9
Gavin Hartnett <gshartnett@gmail.com>
2022-04-24 21:39:55 -0700

    added notebooks

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# hierarchical
+# The Hierarchical Parity Model
 Code repository for the paper "The Hierarchical Parity Model: A Computationally Tractable Spin-Glass"
+
+## Introduction
+This repository contains the code used for the numerical analyses of the hierarchical parity model reported on in the paper.
+
+## Contents
+The key contents of this repository are:
+- `emp.jl`: a Julia script containing the functions used to implement the EMP model
+- `EMP Notebook.ipynb` a Jupyter notebook which walks through the math and implementation of the EMP model
+- `EMP Calculations.nb` a Mathematica notebook which contains some useful calculations (relating to Compton scattering and the problem geometry).
+- `Plotting Notebook.ipynb` a Jupyter notebook used for making Python plots of the variation of the EMP peak magnitude over a region on the surface of the Earth.
+- `Seiler Digitized Data` a directory containing digitized data from select figures in the original Seiler report. The data was digitized using [this online tool](https://apps.automeris.io/wpd/).
+
+## Installation
+To clone the repository and install the required Julia and Python packages, follow these instructions. It is recommended to perform the Python installation within an Anaconda virtual environment, but this is not necessary.
+
+Clone the repo:
+```
+git clone git@github.com:gshartnett/karzas-latter-emp.git
+cd karzas-latter-emp
+```
+
+Create a python virtual environment and create a corresponding Jupyter kernel (optional). The Jupyter kernel is required to run the `Plotting Notebook.ipynb` file.
+```
+conda create --name karzas-latter-emp python=3.8
+conda activate karzas-latter-emp
+pip intall ipykernel
+python -m ipykernel install --user --name karzas-latter-emp --display-name "Python (karzas-latter-emp)"
+```
+
+Install the Python packages
+```
+pip install -r requirements.txt
+```
+
+Install the Julia packages (enter this command in the Pkg manager, accessible from the Julia REPL by entering `]`)
+```
+add IJulia DelimitedFiles DifferentialEquations JSON LaTeXStrings Plots Printf QuadGK
+```
+
+Add the Julia kernel to Jupyter (this is necessary to run the `EMP Notebook.ipynb` file). Enter the Julia REPL and enter
+```
+using IJulia
+installkernel("Julia")
+```
+
+## License
+This code is Copyright (C) 2022 RAND Corporation, and provided under the MIT license. See `LICENSE` for more information.
+
+## Contact
+Developed by [Gavin Hartnett](https://www.rand.org/about/people/h/hartnett_gavin_s.html) (email: hartnett@rand.org).
---
+++ utils.py
@@ -0,0 +1,227 @@
+import os
+import numpy as onp #o for "old" or "original"
+
+#os.environ["LD_LIBRARY_PATH"]="/usr/lib/x86_64-linux-gnu/" #this is where the libcudnn.so file resides
+#os.environ["JAX_PLATFORM_NAME"] = "cpu"
+
+import jax.numpy as np
+import jax
+from jax import grad, jit, lax, random, ops, vmap, jacfwd, jacrev, device_get, device_put
+from jax.lib import xla_bridge
+print("jax version {}".format(jax.__version__))
+print("jax backend {}".format(xla_bridge.get_backend().platform))
+
+
+def generate_couplings_dic(n, prob=0.5, sigma=0):
+    '''
+    Generate a realization of the couplings:
+    J_{k,p} = 2^{k \sigma} x, where x is {-1,1} with 
+    probability {1-prob, prob}. 
+    '''
+    couplings = {(k,p): (2**(k*sigma))*(2*onp.random.binomial(1, prob) - 1) for k in range(n+1) for p in range(1,2**(n-k)+1)}
+    return couplings
+
+
+def generate_couplings(n, prob=0.5, sigma=0):
+    '''
+    Generate a realization of the couplings:
+    J_{k,p} = 2^{k \sigma} x, where x is {-1,1} with 
+    probability {1-prob, prob}. 
+    '''
+    couplings = {(k,p): (2**(k*sigma))*(2*onp.random.binomial(1, prob) - 1.0) for k in range(n+1) for p in range(1,2**(n-k)+1)}
+    return np.asarray(list(couplings.values()))
+
+
+def find_parities_dic(beta, couplings):
+    '''
+    Using the recursion relation, compute the parities at 
+    all levels in the hierarchical tree.
+    '''
+
+    n = int(onp.log((len(couplings)+1)/2)/onp.log(2))  
+    parities = {(k,p):0 for k in range(n+1) for p in range(1,2**(n-k)+1)}
+
+    for k in range(0, n+1):
+        for p in range(1,2**(n-k)+1):
+
+            betaJ = beta*couplings[k,p]
+
+            if k == 0:
+                parities[k,p] = onp.tanh(betaJ)
+            else:
+                parities[k,p] = (onp.sinh(betaJ) + onp.cosh(betaJ)*parities[k-1,2*p-1]*parities[k-1,2*p])
+                parities[k,p] /= (onp.cosh(betaJ) + onp.sinh(betaJ)*parities[k-1,2*p-1]*parities[k-1,2*p])
+                
+    return parities
+
+
+def Nishimorhi_beta(prob):
+    '''
+    Compute the Nishimori inverse temperature,
+    \beta = \frac{1}{2} \ln\left( \frac{p}{1-p})
+    '''
+    return 0.5*onp.log(prob/(1-prob))
+
+
+def index_dictionaries(n):
+    '''
+    Find the map and inverse map from the tree indices (k,p)
+    to a 1d array index i
+    '''
+    i = 0
+    i_to_kp = {}
+    kp_to_i = {}
+    for k in range(0, n+1):
+        for p in range(1, 2**(n-k)+1):
+            i_to_kp[i] = (k,p)
+            kp_to_i[(k,p)] = i
+            i += 1
+            
+    return i_to_kp, kp_to_i
+
+
+@jit
+def find_parities(beta, couplings):
+    '''
+    Using the recursion relation, compute the parities at 
+    all levels in the hierarchical tree.
+    Note: this is rather slow.
+    '''
+    
+    n = int(onp.log((len(couplings)+1)/2)/onp.log(2)) 
+    i_to_kp, kp_to_i = index_dictionaries(n)    
+    parities = np.zeros(len(couplings))
+    
+    for k in range(0, n+1):
+        for p in range(1,2**(n-k)+1):
+            i = kp_to_i[(k,p)]
+            
+            betaJ = beta*couplings[i]
+
+            if k == 0:
+                new_value = np.tanh(betaJ)
+                #parities = ops.index_update(parities, i, new_value)
+                parities = parities.at[i].set(new_value)
+            else:
+                i_left = kp_to_i[(k-1,2*p-1)]
+                i_right = kp_to_i[(k-1,2*p)]                
+                new_value = (np.sinh(betaJ) + np.cosh(betaJ)*parities[i_left]*parities[i_right])
+                new_value /= (np.cosh(betaJ) + np.sinh(betaJ)*parities[i_left]*parities[i_right])
+                #parities = ops.index_update(parities, i, new_value)
+                parities = parities.at[i].set(new_value)
+                
+    return parities
+
+
+@jit
+def log_partition_function(beta, couplings):
+    '''
+    Using the recursion relation, compute the log partition function.
+    Note: this is rather slow.
+    Note: I did a quick & dirty check of this against my Mathematica code.
+    '''
+    n = int(onp.log((len(couplings)+1)/2)/onp.log(2))
+    i_to_kp, kp_to_i = index_dictionaries(n)    
+    parities = find_parities(beta, couplings)    
+    lnZ = np.zeros(len(parities))
+
+    for k in range(0, n+1):
+        for p in range(1,2**(n-k)+1):
+            i = kp_to_i[(k,p)]
+            betaJ = beta*couplings[i]
+
+            if k == 0:
+                new_value = np.log(2*np.cosh(betaJ))
+                #lnZ = ops.index_update(lnZ, i, new_value)
+                lnZ = lnZ.at[i].set(new_value)
+            else:
+                i_left = kp_to_i[(k-1,2*p-1)]
+                i_right = kp_to_i[(k-1,2*p)]                
+                new_value = lnZ[i_left] + lnZ[i_right] + np.log(np.cosh(betaJ) + parities[i_left]*parities[i_right]*np.sinh(betaJ))
+                #lnZ = ops.index_update(lnZ, i, new_value)
+                lnZ = lnZ.at[i].set(new_value)                
+    return lnZ
+
+
+@jit
+def dlnZ_by_dJ(beta, couplings):
+    '''
+    Compute the gradient of the log partition function wrt to the couplings
+    '''
+    return grad(lambda x: log_partition_function(beta, x)[-1])(couplings)
+
+
+@jit
+def dlnZ_by_dbeta(beta, couplings):
+    '''
+    Compute the derivative of the log partition function wrt to inverse 
+    temperature
+    '''
+    return grad(lambda x: log_partition_function(x, couplings)[-1])(beta)
+
+
+@jit
+def d2lnZ_by_dbeta(beta, couplings):
+    '''
+    Compute the second derivative of the log partition function wrt to 
+    inverse temperature
+    '''
+    return grad(lambda x: dlnZ_by_dbeta(x, couplings))(beta)
+
+
+@jit 
+def free_energy(beta, couplings):
+    '''Compute the free energy'''
+    return - log_partition_function(beta, couplings)[-1]/beta
+        
+
+@jit
+def energy(beta, couplings):
+    '''Compute the energy'''
+    return - dlnZ_by_dbeta(beta, couplings)
+
+
+@jit
+def specific_heat(beta, couplings):
+    '''
+    Compute the specific heat:
+    C = - T \partial_T^2 F, or 
+    C = (-\beta^3 \partial_{\beta}^2 - 2 \beta^2 \partial_{\beta} F)
+    '''
+    return - beta**3*d2lnZ_by_dbeta(beta, couplings) - 2*beta**2*d2lnZ_by_dbeta(beta, couplings)
+
+
+@jit
+def entropy(beta, couplings):
+    '''Compute the entropy'''
+    return beta*(energy(beta, couplings) - free_energy(beta, couplings))
+
+
+@jit
+def d2lnZ_by_dJ(beta, couplings):
+    '''
+    Compute the second derivative of the log partition function wrt to 
+    the couplings J_{k,p}
+    '''
+    return jax.jacfwd(grad(lambda x: log_partition_function(beta, x)[-1]))(couplings)
+
+
+@jit
+def chi_SG(beta, couplings):
+    '''
+    The spin-glass susceptability
+    \frac{\beta^2}{N} \sum_{ij} (<s_i s_j> - <s_i><s_j>)^2
+    Note: this need an expectation value over the disorder
+    '''
+    n = int(onp.log((len(couplings)+1)/2)/onp.log(2))
+    return np.sum(np.square(d2lnZ_by_dJ(beta, couplings)[0:2**n, 0:2**n]))/2**n
+
+
+@jit
+def chi_ij(beta, couplings):
+    '''
+    The spin-glass susceptability matrix
+    (<s_i s_j> - <s_i><s_j>)
+    '''
+    n = int(onp.log((len(couplings)+1)/2)/onp.log(2))
+    return d2lnZ_by_dJ(beta, couplings)[0:2**n, 0:2**n]/beta**2
